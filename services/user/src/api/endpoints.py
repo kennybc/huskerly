500df7a5b57ea7cff534dc29ac4d5902415f69ce
Nick <tietje.n@northeastern.edu>
2024-07-29 11:28:35 -0400

    migrate to new repo

---
+++ services/user/src/api/endpoints.py
@@ -0,0 +1,105 @@
+from fastapi import APIRouter, HTTPException, Depends, Header, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
+from core.user import get_all_users_from_userpool, get_user_from_userpool, get_user_permission_level, request_org, update_org_request, list_invites, join_org, invite_org
+
+router = APIRouter()
+
+def get_session_token(session_token: str = Header(...)):
+    if not session_token:
+        raise HTTPException(status_code=400, detail="Session token missing")
+    # TODO: add logic to validate the session token here
+    return session_token
+
+
+@router.get("/users", response_model=List[dict])
+def get_all_users():
+    try:
+        users = get_all_users_from_userpool()
+        return users
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+class UserRequest(BaseModel):
+    user_email: str
+    
+@router.get("/users/{user_email}", response_model=dict)
+def get_user(request: UserRequest): # TODO: use session token , session_token: str = Depends(get_session_token)
+    try:
+        user = get_user_from_userpool(request.user_email)
+        return user
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+class PermissionRequest(BaseModel):
+    org_id: Optional[int] = None
+    
+@router.get("/users/{user_email}/permission", response_model=str)
+def get_permission(user_email: str, request: PermissionRequest): #TODO: should use session token
+    try:
+        permission = get_user_permission_level(user_email, request.org_id)
+        return permission
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+class OrgCreateRequest(BaseModel):
+    org_name: str
+    creator_email: str
+    
+@router.post("/org/request", response_model=str)
+def request_organization(request: OrgCreateRequest):
+    try:
+        status = request_org(request.org_name, request.creator_email)
+        return status
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+class OrgApproveRequest(BaseModel):
+    org_name: str
+    current_user_email: str #TODO: should use session token
+    creator_email: str
+
+@router.put("/org/request", response_model=str)
+def update_organization_request(request: OrgApproveRequest):
+    try:
+        status = update_org_request(
+            request.org_name, request.current_user_email, request.status)
+        return status
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/invites/{user_email}", response_model=List[dict])
+def list_user_invites(user_email: str):
+    try:
+        invites = list_invites(user_email)
+        return invites
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+class JoinRequest(BaseModel):
+    org_id: int
+    user_email: str
+
+@router.post("/org/join", response_model=int)
+def join_organization(request: JoinRequest):
+    try:
+        org_id = join_org(request.org_id, request.user_email)
+        return org_id
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+class InviteRequest(BaseModel):
+    org_id: int
+    invitee_email: str
+    inviter_email: str
+    lifetime: Optional[int] = 86400
+    
+@router.post("/org/invite", response_model=int)
+def invite_to_organization(invite_request: InviteRequest):
+    try:
+        org_id = invite_org(invite_request.org_id, invite_request.invitee_email,
+                            invite_request.inviter_email, invite_request.lifetime)
+        return org_id
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ services/user/src/utils/connect.py
@@ -0,0 +1,30 @@
+from psycopg2 import pool
+from secrets import get_aws_secret
+
+
+def init_connection_pool(dbname):
+    secret_name = "huskerly_credentials"
+    credentials = get_aws_secret(secret_name)
+
+    connection_pool = pool.SimpleConnectionPool(
+        1,  # Minimum number of connections
+        20,  # Maximum number of connections
+        dbname=dbname,
+        user=credentials['username'],
+        password=credentials['password'],
+        host=credentials['host'],
+        sslmode='require',
+    )
+    return connection_pool
+
+
+invites_connection_pool = init_connection_pool("huskerlyinvites")
+
+
+def connect_to_invites_database():
+    global invites_connection_pool
+
+    if not invites_connection_pool:
+        invites_connection_pool = init_connection_pool("huskerlyinvites")
+    conn = invites_connection_pool.getconn()
+    return conn
