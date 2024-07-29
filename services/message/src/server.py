d6a5e33eb1589be18726de540c551c6200a672ff
kennybc <54950614+kennybc@users.noreply.github.com>
2024-07-29 16:52:20 -0400

    basic socket endpoint

--- services/message/src/server.py
+++ services/message/src/server.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from sockets import ConnectionManager
 
 app = FastAPI()
-
+manager = ConnectionManager()
 
 @app.get("/")
 def get_root():
     return {"name": "ms-message", "data": "3"}
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
