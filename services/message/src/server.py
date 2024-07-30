70e0c3887a621a08c9251f67290811d309bf3b90
kennybc <54950614+kennybc@users.noreply.github.com>
2024-07-30 12:01:05 -0400

    switch ws routes to POST

--- services/message/src/server.py
+++ services/message/src/server.py
@@ -7,17 +7,17 @@ connections = []
 def get_root():
     return {"name": "ms-message", "data": "3"}
 
-@app.get("/ws/connect")
+@app.post("/ws/connect")
 async def ws_connect(req: Request):
     body = await req.json()
     print(body)
 
-@app.get("/ws/disconnect")
+@app.post("/ws/disconnect")
 async def ws_disconnect(req: Request):
     body = await req.json()
     print(body)
 
-@app.get("/ws/send")
+@app.post("/ws/send")
 async def ws_send(req: Request):
     body = await req.json()
     print(body)
