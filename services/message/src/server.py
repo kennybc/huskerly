f1ce67ca5e314512557a20c60211a937f6fe06ae
kennybc <54950614+kennybc@users.noreply.github.com>
2024-07-31 15:51:19 -0400

    rm unused

--- services/message/src/server.py
+++ services/message/src/server.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI
 
 from dms import routes as dm_routes
-from src.utils.connect import initialize_db_connection
 
 app = FastAPI(root_path="/message", debug=True)
 app.include_router(dm_routes.router)
-
-initialize_db_connection()
 
 
 @app.get("/")
