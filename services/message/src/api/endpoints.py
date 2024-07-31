60e730e804b34bab941b24c76ad8fb3462fce784
Nick <tietje.n@northeastern.edu>
2024-07-31 16:03:43 -0400

    comment out buggy code

--- services/message/src/api/endpoints.py
+++ services/message/src/api/endpoints.py
@@ -7,24 +7,24 @@ from core.organization import register_org
 router = APIRouter()
 
 
-@router.get("/orgs", response_model=List[dict])
-def get_all_orgs():
-    try:
-        return []
-    except Exception as e:
-        raise HTTPException(
-            status_code=500, detail=f"""Error getting all orgs: {str(e)}""")
+# @router.get("/orgs", response_model=List[dict])
+# def get_all_orgs():
+#     try:
+#         return []
+#     except Exception as e:
+#         raise HTTPException(
+#             status_code=500, detail=f"""Error getting all orgs: {str(e)}""")
 
 
-class OrgCreateRequest(BaseModel):
-    org_name: str
-    creator_email: str
+# class OrgCreateRequest(BaseModel):
+#     org_name: str
+#     creator_email: str
 
 
-@router.post("/orgs", response_model=int)
-def create_org(request: OrgCreateRequest):
-    try:
-        return register_org(request.org_name, request.creator_email)
-    except Exception as e:
-        raise HTTPException(
-            status_code=500, detail=f"""Error registering org: {str(e)}""")
+# @router.post("/orgs", response_model=int)
+# def create_org(request: OrgCreateRequest):
+#     try:
+#         return register_org(request.org_name, request.creator_email)
+#     except Exception as e:
+#         raise HTTPException(
+#             status_code=500, detail=f"""Error registering org: {str(e)}""")
