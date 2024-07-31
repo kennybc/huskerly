dc9dd4d0f0115f5c64be381dee4fefcdfda3c5e7
Nick <tietje.n@northeastern.edu>
2024-07-31 15:55:44 -0400

    fix server setup

--- services/message/src/server.py
+++ services/message/src/server.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 
 from dms import routes as dm_routes
+from api import endpoints
 from utils.connect import initialize_db_connection
 
 app = FastAPI(root_path="/message", debug=True)
 app.include_router(dm_routes.router)
+app.include_router(endpoints.router)
+
+initialize_db_connection()
 
 
 @app.get("/")
