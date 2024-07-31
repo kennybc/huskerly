6e3b22f8a11de82f599dfb17f1b2956e5c3128c3
Nick <tietje.n@northeastern.edu>
2024-07-31 15:08:07 -0400

    re-work connection pool

--- services/user/src/utils/connect.py
+++ services/user/src/utils/connect.py
@@ -2,49 +2,35 @@ import mysql.connector
 from mysql.connector import pooling
 from utils.aws import get_aws_secret
 
+invites_connection_pool = None
+
 
 def init_connection_pool(dbname):
     secret_name = "huskerly-db-credentials"
     credentials = get_aws_secret(secret_name)
     try:
-        connection_pool = pooling.MySQLConnectionPool(
+        return pooling.MySQLConnectionPool(
             pool_name="mypool",
-            pool_size=20,  # Maximum number of connections
+            pool_size=16,  # Maximum number of connections
             pool_reset_session=True,
             database=dbname,
             user=credentials['username'],
             password=credentials['password'],
             host=credentials['host'],
             ssl_disabled=False,
+            connection_timeout=10
         )
     except mysql.connector.Error as err:
-        print(f"Error initializing connection pool: {err}")
-        return None
-    return connection_pool
-
-
-invites_connection_pool = None
+        raise ValueError(f"Error initializing connection pool: {err}")
 
 
 def connect_to_invites_database():
-    # global invites_connection_pool
+    global invites_connection_pool
 
-    # if not invites_connection_pool:
-    #     invites_connection_pool = init_connection_pool("huskerlyinvitesdb")
+    if not invites_connection_pool:
+        invites_connection_pool = init_connection_pool("huskerlymessagingdb")
 
-    # if invites_connection_pool is None:
-    #     raise ValueError("Failed to initialize connection pool")
+    if invites_connection_pool is None:
+        raise ValueError("Failed to initialize connection pool")
 
-    # conn = invites_connection_pool.get_connection()
-
-    secret_name = "huskerly-db-credentials"
-    credentials = get_aws_secret(secret_name)
-
-    conn = mysql.connector.connect(
-        database="huskerlyinvitesdb",
-        user=credentials['username'],
-        password=credentials['password'],
-        host=credentials['host'],
-        ssl_disabled=False,
-    )
-    return conn
+    return invites_connection_pool.get_connection()
