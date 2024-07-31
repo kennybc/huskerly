ae2d62a945d0184339b5542aa8136f2bae6d21fc
kennybc <54950614+kennybc@users.noreply.github.com>
2024-07-31 09:52:46 -0400

    change request body parsing

--- services/message/src/server.py
+++ services/message/src/server.py
@@ -1,4 +1,5 @@
-from fastapi import Request, FastAPI
+from typing import Any
+from fastapi import Body, FastAPI
 
 app = FastAPI(root_path="/message")
 connections = []
@@ -8,25 +9,21 @@ def get_root():
     return {"name": "ms-message", "data": "4"}
 
 @app.post("/ws/connect")
-async def ws_connect(req: Request):
-    body = await req.json()
-    print(body)
+async def ws_connect(req: Any = Body(None)):
+    print(req)
     return {"status": 200}
 
 @app.post("/ws/disconnect")
-async def ws_disconnect(req: Request):
-    body = await req.json()
-    print(body)
+async def ws_disconnect(req: Any = Body(None)):
+    print(req)
     return {"status": 200}
 
 @app.post("/ws/unknown")
-async def ws_unknown(req: Request):
-    body = await req.json()
-    print(body)
+async def ws_unknown(req: Any = Body(None)):
+    print(req)
     return {"status": 404}
 
 @app.post("/ws/send")
-async def ws_send(req: Request):
-    body = await req.json()
-    print(body)
+async def ws_send(req: Any = Body(None)):
+    print(req)
     return {"status": 200}
