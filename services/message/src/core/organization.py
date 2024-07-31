3e88db7d2917deed5f73e67b365feedc7b441c86
Nick <tietje.n@northeastern.edu>
2024-07-31 16:18:51 -0400

    remove user-related code

--- services/message/src/core/organization.py
+++ services/message/src/core/organization.py
@@ -1,9 +1,6 @@
 
 
-from utils.aws import get_aws_secret, get_session
 from utils.connect import get_cursor
-
-pool_id = get_aws_secret("huskerly_userpool_id")["id"]
 
 
 def register_org(org_name, creator_email):
@@ -20,30 +17,30 @@ def register_org(org_name, creator_email):
             cursor.execute("SELECT LAST_INSERT_ID()")
             org_id = cursor.fetchone()[0]
 
-        session = get_session()
-        client = session.client('cognito-idp', region_name='us-east-2')
+        # session = get_session()
+        # client = session.client('cognito-idp', region_name='us-east-2')
 
-        response = client.admin_update_user_attributes(
-            UserPoolId=pool_id,
-            Username=creator_email,
-            UserAttributes=[
-                {
-                    'Name': 'custom:UserStatus',
-                    'Value': 'JOINED'
-                },
-                {
-                    'Name': 'custom:OrgId',
-                    'Value': str(org_id)
-                },
-                {
-                    'Name': 'custom:OrgRoll',  # TODO: NEEDS TO BE FIXED
-                    'Value': 'MEMBER'
-                }
-            ]
-        )
+        # response = client.admin_update_user_attributes(
+        #     UserPoolId=pool_id,
+        #     Username=creator_email,
+        #     UserAttributes=[
+        #         {
+        #             'Name': 'custom:UserStatus',
+        #             'Value': 'JOINED'
+        #         },
+        #         {
+        #             'Name': 'custom:OrgId',
+        #             'Value': str(org_id)
+        #         },
+        #         {
+        #             'Name': 'custom:OrgRoll',  # TODO: NEEDS TO BE FIXED
+        #             'Value': 'MEMBER'
+        #         }
+        #     ]
+        # )
 
-        if response.get('ResponseMetadata', {}).get('HTTPStatusCode') != 200:
-            raise Exception(f"""Failed to update user {
-                            creator_email} with organization {org_id} in Cognito.""")
+        # if response.get('ResponseMetadata', {}).get('HTTPStatusCode') != 200:
+        #     raise Exception(f"""Failed to update user {
+        #                     creator_email} with organization {org_id} in Cognito.""")
 
         return org_id
