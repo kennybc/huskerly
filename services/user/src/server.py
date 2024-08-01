d9a26ddfd3e5d38c313fd7e44bf858c4518950bd
Nick <tietje.n@northeastern.edu>
2024-08-01 16:02:47 -0400

    add cors middleware

--- services/user/src/server.py
+++ services/user/src/server.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api import endpoints
 from utils.connect import initialize_db_connection
 
 app = FastAPI(root_path="/user", debug=True)
+
+origins = [
+    "http://localhost:3000",
+    "https://master.d2a8ctqnn4cl7u.amplifyapp.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(endpoints.router)
 
