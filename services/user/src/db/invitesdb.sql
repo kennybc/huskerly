f7999dcb725f3b3ba8989dde7dc6192b357a42d9
Nick <tietje.n@northeastern.edu>
2024-08-01 13:51:39 -0400

    fix schema

--- services/user/src/db/invitesdb.sql
+++ services/user/src/db/invitesdb.sql
@@ -5,8 +5,8 @@ CREATE DATABASE huskerlyinvitesdb;
 USE huskerlyinvitesdb;
 
 CREATE TABLE organization_invites (
-    user_email UNIQUE VARCHAR(255),
-    org_id INT,
+    user_email VARCHAR(255) UNIQUE NOT NULL,
+    org_id BIGINT UNSIGNED NOT NULL,
     created_by_email VARCHAR(255) NOT NULL,
     created_date TIMESTAMP DEFAULT NOW(),
     expiration_date TIMESTAMP,
