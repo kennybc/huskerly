153a845eb911d52b4db54e9a5a78769535c82d9a
Nick <tietje.n@northeastern.edu>
2024-08-02 15:56:17 -0400

    fix bug

--- services/message/src/core/organization.py
+++ services/message/src/core/organization.py
@@ -18,8 +18,7 @@ def get_perm_level(user_email: str, org_id: Optional[int] = None) -> str:
         perm_level = requests.get(
             user_perm_endpoint + f"{user_email}/{org_id}")
     print("perm_level json: ", perm_level.json())
-    print("perm_level: ", perm_level)
-    return perm_level
+    return perm_level.json()
 
 
 def check_assist_admin_perm(current_user_email: str, org_id: Optional[int] = None) -> bool:
