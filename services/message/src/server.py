a5e0fe120aad82e43b5922f99f608a2c6e7e26bb
Nick <tietje.n@northeastern.edu>
2024-08-02 13:47:20 -0400

    fix bad import

--- services/message/src/server.py
+++ services/message/src/server.py
@@ -2,13 +2,11 @@ from fastapi import FastAPI
 
 from dms import routes as dm_routes
 from api import post_routes
-from api import endpoints
 from utils.connect import initialize_db_connection
 
 app = FastAPI(root_path="/message", debug=True)
 app.include_router(dm_routes.router)
 app.include_router(post_routes.router)
-app.include_router(endpoints.router)
 
 initialize_db_connection()
 
