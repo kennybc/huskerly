4432f096aaf62c5782e828e88002241c76687fa4
Nick <tietje.n@northeastern.edu>
2024-08-02 13:52:30 -0400

    fix router usage

--- services/message/src/server.py
+++ services/message/src/server.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 
 from dms import routes as dm_routes
-from api import post_routes
+from api import org_routes, team_routes, post_routes
 from utils.connect import initialize_db_connection
 
 app = FastAPI(root_path="/message", debug=True)
 app.include_router(dm_routes.router)
 app.include_router(post_routes.router)
+app.include_router(org_routes.router)
+app.include_router(team_routes.router)
 
 initialize_db_connection()
 
