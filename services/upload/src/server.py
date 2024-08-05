f7a96baac967585f927a63791726e53556c478c0
kennybc <54950614+kennybc@users.noreply.github.com>
2024-08-05 11:39:03 -0400

    upload files to s3 + uuid keys

--- services/upload/src/server.py
+++ services/upload/src/server.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
+import os
+import boto3
+import uuid
+from fastapi import File, UploadFile, Form, FastAPI
+from typing import List
+
 
 app = FastAPI()
+
+bucket = boto3.resource("s3").Bucket("huskerly-attachments")
 
 
 @app.get("/")
 def get_root():
-    return {"name": "ms-upload", "data": "3"}
+    return {"name": "ms-upload", "data": "4"}
+
+
+@app.post("/")
+def post_attachment(files: List[UploadFile] = File(...)):
+    keys = []
+    for file in files:
+        key = str(uuid.uuid4().hex)
+        try:
+            bucket.put_object(
+                Key=key,
+                Body=file.file.read(),
+                ContentType=file.headers["content-type"],
+            )
+            keys.append(key)
+        except Exception as e:
+            print(f"Failed to upload file ({file.filename}): {e}")
+            return {"Status": "FAILED"}
+
+    return {"Status": "SUCCESS", "Keys": keys}
