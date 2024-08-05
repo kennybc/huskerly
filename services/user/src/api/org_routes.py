175dff63a7ca89803f6af3f4a441e7379ef3d630
Nick <tietje.n@northeastern.edu>
2024-08-05 16:00:24 -0400

    clean up comments

--- services/user/src/api/org_routes.py
+++ services/user/src/api/org_routes.py
@@ -8,12 +8,8 @@ router = APIRouter(prefix="/org")
 
 @router.get("/requests", response_model=dict)
 def get_org_requests():
-    # try:
     requests = user.list_org_requests()
     return {'Status': 'SUCCESS', 'Requests': requests}
-    # except Exception as e:
-    #     raise HTTPException(
-    #         status_code=500, detail=f"""Error sending organization request: {str(e)}""")
 
 
 class OrgCreateRequest(BaseModel):
@@ -23,12 +19,8 @@ class OrgCreateRequest(BaseModel):
 
 @router.post("/request", response_model=dict)
 def request_organization(request: OrgCreateRequest):
-    # try:
     user.request_org(request.org_name, request.creator_email)
     return {'Status': 'SUCCESS'}
-    # except Exception as e:
-    #     raise HTTPException(
-    #         status_code=500, detail=f"""Error sending organization request: {str(e)}""")
 
 
 class OrgApproveRequest(BaseModel):
@@ -40,14 +32,10 @@ class OrgApproveRequest(BaseModel):
 
 @router.put("/request", response_model=dict)
 def update_organization_request(request: OrgApproveRequest):
-    # try:
     user.update_org_request(
         request.org_name, request.creator_email,
         request.current_user_email, request.status)
     return {'Status': 'SUCCESS'}
-    # except Exception as e:
-    #     raise HTTPException(
-    #         status_code=500, detail=f"""Error updating organization request: {str(e)}""")
 
 
 class JoinRequest(BaseModel):
@@ -57,12 +45,8 @@ class JoinRequest(BaseModel):
 
 @router.post("/join", response_model=dict)
 def join_organization(request: JoinRequest):
-    # try:
     user.join_org(request.org_id, request.user_email)
     return {'Status': 'SUCCESS'}
-    # except Exception as e:
-    #     raise HTTPException(
-    #         status_code=500, detail=f"""Error joining organization: {str(e)}""")
 
 
 class InviteRequest(BaseModel):
@@ -74,23 +58,15 @@ class InviteRequest(BaseModel):
 
 @router.post("/invite", response_model=dict)
 def invite_to_organization(invite_request: InviteRequest):
-    # try:
     user.invite_org(invite_request.org_id, invite_request.invitee_email,
                     invite_request.inviter_email, invite_request.lifetime)
     return {'Status': 'SUCCESS'}
-    # except Exception as e:
-    #     raise HTTPException(
-    #         status_code=500, detail=f"""Error inviting to organization: {str(e)}""")
 
 
 @router.get("/{org_id}", response_model=dict)
 def get_all_users(org_id: int):
-    # try:
     users = user.get_all_users_from_userpool_with_org_id(org_id)
     return {'Status': 'SUCCESS', 'Users': users}
-    # except Exception as e:
-    #     raise HTTPException(
-    #         status_code=500, detail=f"""Error getting all users from organization: {str(e)}""")
 
 
 class PromoteRequest(BaseModel):
@@ -100,13 +76,9 @@ class PromoteRequest(BaseModel):
 
 @router.put("/{org_id}/promote", response_model=dict)
 def promote_user(org_id: int, request: PromoteRequest):
-    # try:
     user.promote_user(
         org_id, request.user_email, request.target_role)
     return {'Status': 'SUCCESS'}
-    # except Exception as e:
-    #     raise HTTPException(
-    #         status_code=500, detail=f"""Error promoting user: {str(e)}""")
 
 
 class DemoteRequest(BaseModel):
@@ -115,9 +87,5 @@ class DemoteRequest(BaseModel):
 
 @router.put("/{org_id}/demote", response_model=dict)
 def demote_user(org_id: int, request: DemoteRequest):
-    # try:
     user.demote_to_member(org_id, request.user_email)
     return {'Status': 'SUCCESS'}
-    # except Exception as e:
-    #     raise HTTPException(
-    #         status_code=500, detail=f"""Error demoting user to member: {str(e)}""")
--- services/user/src/api/user_routes.py
+++ services/user/src/api/user_routes.py
@@ -12,43 +12,20 @@ def get_session_token(session_token: str = Header(...)):
     return session_token
 
 
-# @router.get("/users/{user_email}", response_model=dict)
-# # TODO: use session token , session_token: str = Depends(get_session_token)
-# def get_user(user_email: str):
-#     try:
-#         user = get_user_from_userpool(user_email)
-#         return user
-#     except Exception as e:
-#         raise HTTPException(
-#             status_code=500, detail=f"""Error getting user information: {str(e)}""")
-
-
 @router.get("/permission/{user_email}", response_model=dict)
 def get_permission(user_email):
-    # try:
     permission = user.get_user_permission_level(user_email)
     return {'Status': 'SUCCESS', 'Permission': permission}
-    # except Exception as e:
-    #     raise HTTPException(
-    #         status_code=500, detail=f"""Error authenticating user permissions: {str(e)}""")
 
 
 @router.get("/permission/{user_email}/{org_id}", response_model=dict)
 # TODO: should use session token
 def get_permission(user_email: str, org_id: int):
-    # try:
     permission = user.get_user_permission_level(user_email, org_id)
     return {'Status': 'SUCCESS', 'Permission': permission}
-    # except Exception as e:
-    #     raise HTTPException(
-    #         status_code=500, detail=f"""Error authenticating user permissions: {str(e)}""")
 
 
 @router.get("/invites/{user_email}", response_model=dict)
 def list_user_invites(user_email: str):
-    # try:
     invites = user.list_invites(user_email)
     return {'Status': 'SUCCESS', 'Invites': invites}
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=f"""Error fetching invites for user {
-    #                         user_email}: {str(e)}""")
