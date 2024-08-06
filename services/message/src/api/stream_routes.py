6df0c3fc9a60c87787c3e505b9284051cb34562a
Nick <tietje.n@northeastern.edu>
2024-08-06 14:53:10 -0400

    add private/public tags

--- services/message/src/api/stream_routes.py
+++ services/message/src/api/stream_routes.py
@@ -8,12 +8,12 @@ router = APIRouter(prefix="/stream")
 class StreamGetRequest(BaseModel):
     current_user_email: str
 
-@router.get("/{stream_id}/messages", response_model=dict)
+@router.get("/{stream_id}/messages", response_model=dict, tags=['Public'])
 def get_posts(stream_id: int, request: StreamGetRequest):
     posts = chat.get_posts(request.current_user_email, stream_id)
     return {'Status': 'SUCCESS', 'Posts': posts}
 
-@router.get("/{stream_id}", response_model=dict)
+@router.get("/{stream_id}", response_model=dict, tags=['Public'])
 def get_stream(stream_id: int, request: StreamGetRequest):
     stream_data = stream.get_stream(request.current_user_email, stream_id)
     return {'Status': 'SUCCESS', 'Data': stream_data}
@@ -22,7 +22,7 @@ class JoinStreamRequest(BaseModel):
     user_email: str
 
 
-@router.post("/{stream_id}/join", response_model=dict)
+@router.post("/{stream_id}/join", response_model=dict, tags=['Public'])
 def join_stream(stream_id: int, request: JoinStreamRequest):
     chat.join_chat(stream_id, request.user_email)
     return {'Status': 'SUCCESS'}
@@ -32,7 +32,7 @@ class StreamDeleteRequest(BaseModel):
     current_user_email: str
 
 
-@router.post("/{stream_id}/delete", response_model=dict)
+@router.post("/{stream_id}/delete", response_model=dict, tags=['Public'])
 def delete_stream(stream_id: int, request: StreamDeleteRequest):
     stream.delete_stream(request.current_user_email, stream_id)
     return {'Status': 'SUCCESS'}
@@ -43,7 +43,7 @@ class StreamCreateRequest(BaseModel):
     team_id: int
 
 
-@router.post("", response_model=dict)
+@router.post("", response_model=dict, tags=['Public'])
 def create_stream(request: StreamCreateRequest):
     stream_id = stream.create_stream(request.stream_name, request.creator_email, request.team_id)
     return {'Status': 'SUCCESS', "stream_id": stream_id}
@@ -55,7 +55,7 @@ class StreamEditRequest(BaseModel):
     public: bool
 
 
-@router.put("/{stream_id}", response_model=dict)
+@router.put("/{stream_id}", response_model=dict, tags=['Public'])
 def edit_stream(stream_id: int, request: StreamEditRequest):
     stream.edit_stream(request.current_user_email, stream_id, request.stream_name, request.public)
     return {'Status': 'SUCCESS'}
@@ -66,7 +66,7 @@ class StreamLeaveRequest(BaseModel):
     user_email: str
 
 
-@router.delete("/{stream_id}", response_model=dict)
+@router.delete("/{stream_id}", response_model=dict, tags=['Public'])
 def leave_stream(stream_id: int, request: StreamLeaveRequest):
     stream.leave_stream(stream_id, request.current_user_email, request.user_email)
     return {'Status': 'SUCCESS'}
