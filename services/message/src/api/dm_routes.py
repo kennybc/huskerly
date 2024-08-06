1608b6b535e5d9883d77c0a56edd997863d88f66
Nick <tietje.n@northeastern.edu>
2024-08-06 14:49:41 -0400

    add public/private tags

--- services/message/src/api/dm_routes.py
+++ services/message/src/api/dm_routes.py
@@ -9,13 +9,13 @@ router = APIRouter(prefix="/dm")
 class DmGetRequest(BaseModel):
     current_user_email: str
 
-@router.get("/{dm_id}/messages", response_model=dict)
+@router.get("/{dm_id}/messages", response_model=dict, tags=['Public'])
 def get_posts(dm_id: int, request: DmGetRequest):
     posts = chat.get_posts(request.current_user_email, dm_id)
     return {'Status': 'SUCCESS', 'Posts': posts}
 
     
-@router.get("/{dm_id}", response_model=dict)
+@router.get("/{dm_id}", response_model=dict, tags=['Public'])
 def get_dm(dm_id: int, request: DmGetRequest):
     dm_data = dm.get_dm(request.current_user_email, dm_id)
     return {'Status': 'SUCCESS', 'Data': dm_data}
@@ -27,7 +27,7 @@ class CreateDmRequest(BaseModel):
     org_id: int
 
 
-@router.post("", response_model=dict)
+@router.post("", response_model=dict, tags=['Public'])
 def create_dm(request: CreateDmRequest):
     dm_id = dm.create_dm(request.current_user_email, request.other_user_email, request.org_id)
     return {'Status': 'SUCCESS', "dm_id": dm_id}
--- services/message/src/api/org_routes.py
+++ services/message/src/api/org_routes.py
@@ -8,7 +8,7 @@ router = APIRouter(prefix="/org")
 class OrgGetAllRequest(BaseModel):
     current_user_email: str
     
-@router.get("", response_model=dict)
+@router.get("", response_model=dict, tags=['Public'])
 def get_all_orgs(request: OrgGetAllRequest):
     orgs = organization.get_all_orgs(request.current_user_email)
     return {'Status': 'SUCCESS', 'Data': orgs}
@@ -18,7 +18,7 @@ class OrgCreateRequest(BaseModel):
     creator_email: str
 
 
-@router.post("", response_model=dict)
+@router.post("", response_model=dict, tags=['Private'])
 def create_org(request: OrgCreateRequest):
     org_id = organization.create_org(request.org_name, request.creator_email)
     return {'Status': 'SUCCESS', "org_id": org_id}
@@ -30,7 +30,7 @@ class TransferOrgRequest(BaseModel):
     current_user_email: str
 
 
-@router.put("/{org_id}/transfer", response_model=dict)
+@router.put("/{org_id}/transfer", response_model=dict, tags=['Public'])
 def transfer_lead_admin(org_id: int, request: TransferOrgRequest):
     organization.transfer_lead_admin(
         org_id, request.new_lead_admin_email, request.current_user_email)
@@ -40,7 +40,7 @@ class DemoteRequest(BaseModel):
     demoted_user_email: str
     current_user_email: str
 
-@router.put("/{org_id}/demote", response_model=dict)
+@router.put("/{org_id}/demote", response_model=dict, tags=['Public'])
 def demote_to_member(org_id: int, request: DemoteRequest):
     organization.demote_to_member(
         org_id, request.demoted_user_email, request.current_user_email)
@@ -52,7 +52,7 @@ class OrgEditRequest(BaseModel):
     current_user_email: str
 
 
-@router.put("/{org_id}", response_model=dict)
+@router.put("/{org_id}", response_model=dict, tags=['Public'])
 def edit_org(org_id: int, request: OrgEditRequest):
     organization.edit_org(
         org_id, request.current_user_email, request.org_name)
@@ -63,13 +63,13 @@ class OrgDeleteRequest(BaseModel):
     current_user_email: str
 
 
-@router.delete("/{org_id}", response_model=dict)
+@router.delete("/{org_id}", response_model=dict, tags=['Public'])
 def delete_org(org_id: int, request: OrgDeleteRequest):
     organization.delete_org(org_id, request.current_user_email)
     return {'Status': 'SUCCESS'}
 
 
-@router.get("/{org_id}", response_model=dict)
+@router.get("/{org_id}", response_model=dict, tags=['Public'])
 def get_org(org_id: int):
     org_data = organization.get_org(org_id)
     return {'Status': 'SUCCESS', 'Data': org_data}
