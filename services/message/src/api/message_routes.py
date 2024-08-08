32cddc94f72e9a6e3a6c7af9a405ef8affb7bdc0
Alex Angione <111721125+alexangione419@users.noreply.github.com>
2024-08-08 15:31:38 -0400

    remove message ahndler class structure

--- services/message/src/api/message_routes.py
+++ services/message/src/api/message_routes.py
@@ -1,22 +1,21 @@
 from typing import Any
 from fastapi import Body, APIRouter
 
-from core.message import MessageHandler
+from core.message import add_connection, remove_connection, send_to_channel, join_channel
 
 router = APIRouter(prefix="/ws")
 
-message_h = MessageHandler()
 
 @router.post("/connect")
 async def ws_connect(req: Any = Body(None)):
     print("New connection -> " + req["connectionId"])
-    message_h.add_connection(req["connectionId"])
+    add_connection(req["connectionId"])
     return {"status": 200}
 
 
 @router.post("/disconnect")
 async def ws_disconnect(req: Any = Body(None)):
-    message_h.remove_connection(req["connectionId"])
+    remove_connection(req["connectionId"])
     return {"status": 200}
 
 
@@ -29,12 +28,12 @@ async def ws_unknown(req: Any = Body(None)):
 @router.post("/send")
 async def ws_send(req: Any = Body(None)):
     print("\nAttempting message from " + req["connectionId"])
-    message_h.send_to_channel(req["connectionId"], req["payload"]["message"])
+    send_to_channel(req["connectionId"], req["payload"]["message"])
     return {"status" : 200}
 
 
 @router.post("/joinChannel")
 async def ws_joinChan(req: Any = Body(None)):
     # join channel takes the unique channel id, the user's email, and the user's connection id
-    status = message_h.join_channel(req["payload"]["channel_id"], req["payload"]["user_email"], req["connectionId"])
+    status = join_channel(req["payload"]["channel_id"], req["payload"]["user_email"], req["connectionId"])
     return status
