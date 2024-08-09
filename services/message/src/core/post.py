a402a64811fc39c3be714d0fe1b5db046c701281
Nick <tietje.n@northeastern.edu>
2024-08-09 09:25:51 -0400

    MORE prints

--- services/message/src/core/post.py
+++ services/message/src/core/post.py
@@ -104,7 +104,9 @@ async def create_post(
                 row = cursor.fetchone()
                 print("row:", row)
                 if row:
+                    print("row[0]:", row[0])
                     attachment_id = cursor.fetchone()[0]
+                    print("attachment_id:", attachment_id)
                     attachment_ids.append(attachment_id)
                     print("created attachment: ", attachment_id)
 
